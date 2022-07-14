7dd6da7d9196db9b30ddb2ae5b03ff68007a3421
BobcatBuckley <98860466+BobcatBuckley@users.noreply.github.com>
2022-07-13 21:12:59 -0400

    second half of project

--- jammming/src/components/Track/Track.js
+++ jammming/src/components/Track/Track.js
@@ -1,34 +1,49 @@
 import React from "react";
-import './Track.css';
+import "./Track.css";
 
-class Track extends React.Component{
-    constructor(props){
-        super(props)
-        this.addTrack = this.addTrack.bind(this);
+class Track extends React.Component {
+  constructor(props) {
+    super(props);
+    this.addTrack = this.addTrack.bind(this);
+    this.removeTrack = this.removeTrack.bind(this);
+  }
+  addTrack() {
+    this.props.onAdd(this.props.track);
+  }
 
+  removeTrack() {
+    this.props.onRemove(this.props.track);
+  }
+
+  renderAction() {
+    if (this.props.isRemoval) {
+      return (
+        <button className="Track-Action" onClick={this.removeTrack}>
+          -
+        </button>
+      );
+    } else {
+      return (
+        <button className="Track-Action" onClick={this.addTrack}>
+          +
+        </button>
+      );
     }
+  }
 
-    renderAction(){
-        if (this.props.isRemoval) {
-            return <button className="Track-Action">-</button>
-        } else {
-            return <button className="Track-Action" 
-            onClick={this.addTrack}>+</button>
-        }
-    }
-
-    render(){
-        console.log(this.addTrack())
-        return(
-    <div className="Track">
+  render() {
+    return (
+      <div className="Track">
         <div className="Track-information">
-            <h3>{this.props.track.name}</h3>
-            <p>{this.props.track.artist} | {this.props.track.album} </p>
+          <h3>{this.props.track.name}</h3>
+          <p>
+            {this.props.track.artist} | {this.props.track.album}{" "}
+          </p>
         </div>
         {this.renderAction()}
-    </div>
-        )
-    }
+      </div>
+    );
+  }
 }
 
 export default Track;
--- jammming/src/components/TrackList/TrackList.js
+++ jammming/src/components/TrackList/TrackList.js
@@ -1,32 +1,25 @@
 import React from "react";
-import './TrackList.css';
+import "./TrackList.css";
 import Track from "../Track/Track";
 
-class TrackList extends React.Component{
-    renderAction(){
-        if (this.props.isRemoval){
-            return <button className="Track-action">-</button>
-        } else {
-            return <button className="Track-action">+</button>
-        }
-    }
-
-    addTrack(){
-        this.props.onAdd(this.props.track);
-    }
-    render(){
-        return(
-            <div className="TrackList" >
-                {
-                    this.props.tracks.map(track => {
-                        return <Track track={track} key={track.id} 
-                        onAdd={this.props.onAdd} />
-                    })
-                }
-                </div>
-        )
-        
-    }
+class TrackList extends React.Component {
+  render() {
+    return (
+      <div className="TrackList">
+        {this.props.tracks.map((track) => {
+          return (
+            <Track
+              track={track}
+              key={track.id}
+              onAdd={this.props.onAdd}
+              onRemove={this.props.onRemove}
+              isRemoval={this.props.isRemoval}
+            />
+          );
+        })}
+      </div>
+    );
+  }
 }
 
 export default TrackList;
